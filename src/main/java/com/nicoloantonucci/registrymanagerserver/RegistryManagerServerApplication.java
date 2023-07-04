df05faceceba4ee56a0904c2a037fdc71beb30ce
Nicolò Antonucci <nicolo.antonucci@i-tel.it>
2023-07-04 09:40:11 +0200

    Java Spring project initalized

---
+++ src/main/java/com/nicoloantonucci/registrymanagerserver/RegistryManagerServerApplication.java
@@ -0,0 +1,13 @@
+package com.nicoloantonucci.registrymanagerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryManagerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryManagerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicoloantonucci/registrymanagerserver/RegistryManagerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicoloantonucci.registrymanagerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryManagerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
